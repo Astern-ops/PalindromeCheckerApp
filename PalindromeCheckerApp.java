dc87c030deedbc9d52d26cb07b777c7dd127d311
DIVYANSHU KANT <krishkant2004@gmail.com>
2026-03-18 14:21:15 +0530

    Add PalindromeCheckerApp.java file

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,5 +1,5 @@
 
-public class UseCase1PalindromeCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("=========================================");
         System.out.println("   Welcome to the Palindrome Checker");
